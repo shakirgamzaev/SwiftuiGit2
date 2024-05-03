11244c722cdd38191fad3364f84ff7628f71f10e
Shakir Gamzaev <shkir100@gmail.com>
2024-05-03 14:23:53 +0200

    Added a button with prominent style to ContentView.swift file

--- SwiftuiGit2/ContentView.swift
+++ SwiftuiGit2/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button {
+                print("hello")
+            } label: {
+                Text("press")
+            }
+            .buttonStyle(.borderedProminent)
+
         }
         .padding()
     }
