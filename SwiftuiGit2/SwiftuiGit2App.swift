5e182994561b4b26e00e290ca36935cfa0674592
Shakir Gamzaev <shkir100@gmail.com>
2024-05-03 14:20:23 +0200

    Initial Commit

---
+++ SwiftuiGit2/SwiftuiGit2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiGit2App.swift
+//  SwiftuiGit2
+//
+//  Created by shakir Gamzaev on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiGit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
